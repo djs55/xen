35607309ebdef8139ca27a4d2d1957f17775eabb
David Scott <dave.scott@citrix.com>
2014-06-25 13:18:47 +0000

    RFC: extend the xenstore ring with a 'closing' signal
    Currently hvmloader uses the xenstore ring and then tries to
    reset it back to its initial state. This is not part of the
    ring protocol and, if xenstored reads the ring while it is
    happening, xenstored will conclude it is corrupted. A corrupted
    ring will prevent PV drivers from connecting. This seems to
    be a rare failure.
    
    Furthermore, when a VM crashes it may jump to a 'crash kernel'
    to create a diagnostic dump. Without the ability to safely
    reset the ring the PV drivers won't be able to reliably
    establish connections, to (for example) stream a memory dump to
    disk.
    
    This prototype patch contains a simple extension of the
    xenstore ring structure, enough to contain version numbers and
    a 'closing' flag. This memory is currently unused within the
    4k page and should be zeroed as part of the domain setup
    process. The oxenstored server advertises version 1, and
    hvmloader detects this and sets the 'closing' flag. The server
    then reinitialises the ring, filling it with obviously invalid
    data to help debugging (unfortunately blocks of zeroes are
    valid xenstore packets) and signals hvmloader by the event
    channel that it's safe to boot the guest OS.
    
    Updates since v2 (thanks to Andy Cooper for the review):
    * hvmloader: use volatile for read of closing flag
    * style improvements
    * remove xenstore version #defines
    
    Updates since v1 (thanks to Paul Durrant for the review):
    * remove unused client version and associated dead code
    * treat 'closing' as a flag by using "!=0" rather than "==1"
    * hvmloader: move function up and remove forward decl
    * document the existing xenstore ring and the extention in misc/
    
    Signed-off-by: David Scott <dave.scott@citrix.com>

--- tools/firmware/hvmloader/xenbus.c
+++ tools/firmware/hvmloader/xenbus.c
@@ -37,6 +37,19 @@ static struct xenstore_domain_interface *rings; /* Shared ring with dom0 */
 static evtchn_port_t event;                     /* Event-channel to dom0 */
 static char payload[XENSTORE_PAYLOAD_MAX + 1];  /* Unmarshalling area */
 
+static void ring_wait(void)
+{
+    struct shared_info *shinfo = get_shared_info();
+    struct sched_poll poll;
+
+    memset(&poll, 0, sizeof(poll));
+    set_xen_guest_handle(poll.ports, &event);
+    poll.nr_ports = 1;
+
+    while ( !test_and_clear_bit(event, shinfo->evtchn_pending) )
+        hypercall_sched_op(SCHEDOP_poll, &poll);
+}
+
 /* Connect our xenbus client to the backend.
  * Call once, before any other xenbus actions. */
 void xenbus_setup(void)
@@ -64,10 +77,15 @@ void xenbus_shutdown(void)
 
     ASSERT(rings != NULL);
 
-    /* We zero out the whole ring -- the backend can handle this, and it's 
-     * not going to surprise any frontends since it's equivalent to never 
-     * having used the rings. */
-    memset(rings, 0, sizeof *rings);
+    if (rings->server_version > 0) {
+        rings->closing = 1;
+        while (*(volatile uint32_t*)&rings->closing == 1)
+            ring_wait ();
+    } else
+        /* If the backend reads the state while we're erasing it then the
+         * ring state will become corrupted, preventing guest frontends from
+         * connecting. This is rare. */
+        memset(rings, 0, sizeof *rings);
 
     /* Clear the event-channel state too. */
     memset(shinfo->vcpu_info, 0, sizeof(shinfo->vcpu_info));
@@ -75,19 +93,6 @@ void xenbus_shutdown(void)
     memset(shinfo->evtchn_mask, 0, sizeof(shinfo->evtchn_mask));
 
     rings = NULL;
-}
-
-static void ring_wait(void)
-{
-    struct shared_info *shinfo = get_shared_info();
-    struct sched_poll poll;
-
-    memset(&poll, 0, sizeof(poll));
-    set_xen_guest_handle(poll.ports, &event);
-    poll.nr_ports = 1;
-
-    while ( !test_and_clear_bit(event, shinfo->evtchn_pending) )
-        hypercall_sched_op(SCHEDOP_poll, &poll);
 }
 
 /* Helper functions: copy data in and out of the ring */
--- xen/include/public/io/xs_wire.h
+++ xen/include/public/io/xs_wire.h
@@ -108,14 +108,19 @@ enum xs_watch_type
  * `incontents 150 xenstore_struct XenStore wire protocol.
  *
  * Inter-domain shared memory communications. */
+
 #define XENSTORE_RING_SIZE 1024
 typedef uint32_t XENSTORE_RING_IDX;
 #define MASK_XENSTORE_IDX(idx) ((idx) & (XENSTORE_RING_SIZE-1))
 struct xenstore_domain_interface {
+    /* XENSTORE_VERSION_0 */
     char req[XENSTORE_RING_SIZE]; /* Requests to xenstore daemon. */
     char rsp[XENSTORE_RING_SIZE]; /* Replies and async watch events. */
     XENSTORE_RING_IDX req_cons, req_prod;
     XENSTORE_RING_IDX rsp_cons, rsp_prod;
+    uint32_t server_version;
+    /* server_version 1 and later: */
+    uint32_t closing;             /* Non-zero means close in progress */
 };
 
 /* Violating this is very bad.  See docs/misc/xenstore.txt. */
